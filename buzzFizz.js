08c3ab8bcf1e942a3bb0a4df87b708203d5fcae1
James Farrell <info@jamespfarrell.com>
2017-08-28 20:51:49 +0200

    outputting markup with simple approach

--- buzzFizz.js
+++ buzzFizz.js
@@ -1,8 +1,16 @@
+let output = "";
+
 for(let i=1;i<=100; i++)
 {
 	let message = "";
     if (i%3==0) message += "Fizz";
 	if (i%5==0) message += "Buzz";
 
-	console.log(message == "" ? i : message);
+	message = message == "" ? i : message;
+	output += "<p>"+message+"</p>";
+	//console.log(message == "" ? i : message);
 }
+
+var div = document.getElementById('output');
+
+div.innerHTML += output;
