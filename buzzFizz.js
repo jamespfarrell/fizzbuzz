a3e1a2705f6d81b88cbc7a68fe5f7931925ca21c
James Farrell <info@jamespfarrell.com>
2017-08-29 14:32:03 +0200

    First working versions

--- buzzFizz.js
+++ buzzFizz.js
@@ -1,15 +1,45 @@
+function actionFactory(action) {
+	return function af({i,messageSoFar}) {
+		messageSoFar = (i%action.factor==0) ? messageSoFar + action.label : messageSoFar;
+		return {
+			i,
+			messageSoFar
+		};
+	}
+}
+
+function compose(...fns) {
+	return function composed(arg) {
+		return fns.reduceRight((result,fn) => fn(result),arg);
+	};
+}
+
+function pipe(...fns) {
+	return compose(...fns.reverse());
+}
+
+const range = [];
+for(var i=1;i<=100;i++){ range.push(i); }
+
 let output = "";
 
-for(let i=1;i<=100; i++)
-{
+let actions = [{ factor: 3 , label: 'Fizz' }, { factor: 5 , label: 'Buzz' }];
+let actionFunctions = [];
+
+actions.forEach(function(action) {
+	actionFunctions.push(actionFactory(action));
+});
+
+let composedFunctions = pipe(...actionFunctions) ;
+
+range.map(function(i){
 	let message = "";
-    if (i%3==0) message += "Fizz";
-	if (i%5==0) message += "Buzz";
+	message = composedFunctions({ i, messageSoFar : "" }).messageSoFar;
+	
 
 	message = message == "" ? i : message;
-	output += "<p>"+message+"</p>";
-	//console.log(message == "" ? i : message);
-}
+	output += "<p>"+message+"</p>"; 
+});
 
 var div = document.getElementById('output');
 
